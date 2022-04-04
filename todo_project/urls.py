5472486cb5fc0080bc2a163e2dec4715961760fc
Anna Nikiforyak <anna_murom33@mail.ru>
2022-04-04 15:21:30 +0300

    added permissions and authentication

--- todo_project/urls.py
+++ todo_project/urls.py
@@ -17,6 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken import views
+
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users.views import UserModelViewSet
 from todo.views import ProjectModelViewSet, ToDoModelViewSet
@@ -31,4 +34,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include(router.urls)),
+    path('api-token-auth/', views.obtain_auth_token),
+    
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
